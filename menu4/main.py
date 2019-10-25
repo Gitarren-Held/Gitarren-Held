42424fe43be5e5e8451c2f0a6a982ba369b7198e
Sebastián Sánchez <ssp013sanchez@gmail.com>
2019-10-25 15:38:51 -0300

    new features

---
+++ menu4/main.py
@@ -0,0 +1,51 @@
+#Módulos
+import random
+import pygame,sys
+from pygame.locals import *
+from classMenu import * #Importamos la claseMenu!
+# Funciones
+def iniciar_solo():
+    print( " Inicia Modo Solo")
+def iniciar_carrera():
+    print (" Inicio Modo Carrera!")
+def creditos():
+    print (" Créditos!!")
+def salir_del_programa():
+    print (" Salir de Guitar Hero.")
+    sys.exit(0)
+# Constantes
+width = 640
+height = 480
+opciones = [("Solo", iniciar_solo),("Carrera", iniciar_carrera),("Creditos", creditos),("Salir", salir_del_programa)]
+
+# ---------------------------------------------------------------------
+# Clases         
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+#Iniciamos el Main!!
+def main():
+    pygame.font.init()
+    screen = pygame.display.set_mode((width, height))
+    fondo = pygame.image.load('images/fondo2.png').convert() 
+    icon = pygame.image.load('images/iconGuitarHero.png') 
+    fondo = pygame.transform.scale(fondo,(width,height))#Escalamos la imagen en Pygame
+    menu = Menu(opciones)
+    menu.inicio1()
+    menu.inicio2()
+    menu.inicio3()
+    pygame.display.set_caption('Guitar Hero') 
+    pygame.display.set_icon(icon)
+    salir = False
+    while not salir:
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                salir = True
+        screen.blit(fondo, (0, 0))
+        menu.actualizar()
+        menu.imprimir(screen)
+        pygame.display.flip()
+        pygame.time.delay(0)
+    return 0
+if __name__ == '__main__':  
+    pygame.init()
+    main()
