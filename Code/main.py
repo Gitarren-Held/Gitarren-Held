38aab80a08f1dddcdfb101c00c80b87ef00a6dc7
NicolasGarciaSantelices <ngarcia1621@gmail.com>
2019-11-07 15:37:13 -0300

    botonera con teclado

--- Code/main.py
+++ Code/main.py
@@ -14,6 +14,7 @@ def main():
     #botonera
     botonera = BotoneraCompleta()
     direccion = '/dev/cu.usbmodem144401'
+    lista = [0,0,0,0,0,0,0,0]
     #se crean dos arreglo
     MatrizLNotas=[]
     #se rellenan estos arreglos con 1000 datos (deberia cargar las lineas de la cancion)
@@ -28,18 +29,44 @@ def main():
         MatrizLNotas.append(LineaN)
     song.play()
     while True:
-        inp = Leer(direccion)
-        reloj.tick(60)
+        #inp = Leer(direccion)
+        reloj.tick(120)
         screen.fill([0,0,0])
         screen.blit(Guitarra,(-315,-180))
         for eventos in pygame.event.get():
+            if eventos.type == pygame.KEYDOWN:
+            # Resuelve que ha sido una tecla de flecha, por lo que
+            # ajusta la velocidad.
+                if eventos.key == pygame.K_z:
+                    lista[0]=1
+                if eventos.key == pygame.K_x:
+                    lista[1]=1
+                if eventos.key == pygame.K_c:
+                    lista[2]=1
+                if eventos.key == pygame.K_v:
+                    lista[3]=1
+                if eventos.key == pygame.K_b:
+                    lista[4]=1
+            if eventos.type == pygame.KEYUP:
+            # Resuelve que ha sido una tecla de flecha, por lo que
+            # ajusta la velocidad.
+                if eventos.key == pygame.K_z:
+                    lista[0]=0
+                if eventos.key == pygame.K_x:
+                    lista[1]=0
+                if eventos.key == pygame.K_c:
+                    lista[2]=0
+                if eventos.key == pygame.K_v:
+                    lista[3]=0
+                if eventos.key == pygame.K_b:
+                    lista[4]=0
             if eventos.type == QUIT:
                 sys.exit(0) 
+        comportamientoBotonera(botonera,lista)
+        drawAll(botonera,screen)
         #for que genera el movimiento en las notas segun cuantas existan en la cancion
         for i in range(0,len(MatrizNotas)):
             movimientolista(MatrizLNotas[i],screen)
-        comportamientoBotonera(botonera,inp)
-        drawAll(botonera,screen)
         pygame.display.flip()
         pygame.display.update()
     return 0 
